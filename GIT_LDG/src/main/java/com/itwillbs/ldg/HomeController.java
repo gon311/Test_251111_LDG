b4934359419bcf89c1411a8ce0d10e0f8b5ae3cc
LDG <gon311@knou.ac.kr>
2026-02-05 11:19:30 +0900

    2222

--- GIT_LDG/src/main/java/com/itwillbs/ldg/HomeController.java
+++ GIT_LDG/src/main/java/com/itwillbs/ldg/HomeController.java
@@ -22,10 +22,9 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	//테스트중
+	//테스트중 2222
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
